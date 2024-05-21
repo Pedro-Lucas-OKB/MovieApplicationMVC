7c65acbe81c167bb3fedd453567f5e5417189d66
Pedro Lucas <pedrogmobile2@gmail.com>
2024-05-20 22:05:29 -0300

    style: added margin bottom in form-group

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,6 +7,7 @@ public class Movie
     public int Id { get; set; }
     public string? Title { get; set; } = default;
     
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; } = default;
